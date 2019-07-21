48d06d20ff6709b6dd0b07e233a58115e6976708
adityaiyengar <aditya@rhrk.uni-kl.de>
2019-07-21 20:30:35 +0200

    Changed the structure for LSTM
    Signed-off-by: adityaiyengar <aditya@rhrk.uni-kl.de>

--- Clothing_artefacts_code_data_tasks/Study_Loosely_Coupling/app/Temp-seq2seqFile.py
+++ Clothing_artefacts_code_data_tasks/Study_Loosely_Coupling/app/Temp-seq2seqFile.py
@@ -3,11 +3,11 @@ import random
 import keras, tensorflow
 from keras.models import Model
 from keras.layers import Input, LSTM, Dense
-
+from keras import regularizers
 inputData = []
 targetData = []
-inputCharacters = set()
-targetCharacters = set()
+inputCharacters = []
+targetCharacters = []
 input_train_data = []
 target_train_data = []
 
@@ -40,89 +40,155 @@ for line in lines:
 
 #partitioning the data to make training set
 for makeTrain in range(len(inputData)):
-  train_range = random.randint(0, 517)
-  if((train_range not in hold_index) and (len(hold_index)<259)):
-    hold_index.append(train_range)
-    input_train_data.append(inputData[train_range])
-    target_train_data.append(targetData[train_range])
+    train_range = random.randint(0, 517)
+    if(train_range not in hold_index) and (len(hold_index) <= len(inputData)/2):
+        hold_index.append(train_range)
+        input_train_data.append(inputData[train_range])
+        target_train_data.append(targetData[train_range])
 
 
 # max_train_input_length= len(max(input_train_data,key=len))
 # max_train_target_legth = len(max(target_train_data,key=len))
+length = 400
 for value in input_train_data:
-    for innerValue in value:
-        if innerValue not in inputCharacters:
-            inputCharacters.add(innerValue)
-
+    for i in range(0, len(value), length):
+        # tempInputList = np.zeros(length)
+        tempInputList = value[i:i+length]
+        tempInputListLength = len(tempInputList)
+        while tempInputListLength < length:
+            tempInputList.insert(tempInputListLength, 0)
+            tempInputListLength += 1
+            # tempInputList[len(tempInputList):length-1] = 0
+            # tempInputList.append(0)
+        inputCharacters.append(tempInputList)
+        # print(np.array(inputCharacters).shape)
+        # if innerValue not in inputCharacters:
+        #     inputCharacters.add(innerValue)
+    # tempInputList = []
+inputCharacters = np.array(inputCharacters)
+inputCharacters = inputCharacters[:, :, np.newaxis]
+print(inputCharacters.shape)
+# input_train_data = array
 for value in target_train_data:
-    for innerValue in value:
-        if innerValue not in targetCharacters:
-            targetCharacters.add(innerValue)
+    for i in range(0, len(value), length):
+        tempOutputList = value[i:i+length]
+        tempOutputListLength = len(tempOutputList)
+        while tempOutputListLength < length:
+            tempOutputList.insert(tempOutputListLength, 0)
+            tempOutputListLength += 1
+        targetCharacters.append(tempOutputList)
 
-max_encoder_seq_length = max([len(txt) for txt in input_train_data])
-max_decoder_seq_length = max([len(txt) for txt in target_train_data])
-num_encoder_tokens = len(inputCharacters)
-num_decoder_tokens = len(targetCharacters)
+targetCharacters = np.array(targetCharacters)
+targetCharacters = targetCharacters[:, :, np.newaxis]
+print(targetCharacters.shape)
 
-print(num_encoder_tokens)
-print(num_decoder_tokens)
-input_token_index = dict(
-  [(char, i) for i, char in enumerate(inputCharacters)])
-target_token_index = dict(
-  [(char, i) for i, char in enumerate(targetCharacters)])
-# print(len(input_token_index))
-
-encoder_input_data = np.zeros(
-  (len(input_train_data), max_encoder_seq_length, num_encoder_tokens),
-  dtype='float32')
-decoder_input_data = np.zeros(
-  (len(input_train_data), max_decoder_seq_length, num_decoder_tokens),
-  dtype='float32')
-decoder_target_data = np.zeros(
-  (len(input_train_data), max_decoder_seq_length, num_decoder_tokens),
-  dtype='float32')
-print(encoder_input_data.shape)
-print(decoder_input_data.shape)
-print(decoder_target_data.shape)
-
-for i, (input_train_data, target_train_data) in enumerate(zip(input_train_data, target_train_data)):
-    for t, char in enumerate(input_train_data):
-        encoder_input_data[i, t, input_token_index[char]] = 1
-    for t, char in enumerate(target_train_data):
-    #     decoder_target_data is ahead of decoder_input_data by one timestep
-        decoder_input_data[i, t, target_token_index[char]] = 1
-        if t > 0:
-        #decoder_target_data will be ahead by one timestep
-        # and will not include the start character.
-            decoder_target_data[i, t - 1, target_token_index[char]] = 1
-
-print(encoder_input_data[2].shape)
-print(decoder_target_data[2].shape)
+# for i, (, target_train_data) in enumerate(zip(input_train_data, target_train_data)):
+#     for t, char in enumerate(input_train_data):
+#         encoder_input_data[i, t, input_token_index[char]] = 1
+#     for t, char in enumerate(target_train_data):
+#     #     decoder_target_data is ahead of decoder_input_data by one timestep
+#         decoder_input_data[i, t, target_token_index[char]] = 1
+#         if t > 0:
+#         #decoder_target_data will be ahead by one timestep
+#         # and will not include the start character.
+#             decoder_target_data[i, t - 1, target_token_index[char]] = 1
+#
+# print(encoder_input_data[2].shape)
+# print(decoder_target_data[2].shape)
 
 batch_size = 64  # batch size for training
 epochs = 100  # number of epochs to train for
-latent_dim = 256  # latent dimensionality of the encoding space
+latent_dim = 256  # latent dimensionality of
 
-encoder_inputs = Input(shape=(None, num_encoder_tokens))
-encoder = LSTM(latent_dim, return_state=True)
-encoder_outputs, state_h, state_c = encoder(encoder_inputs)
-encoder_states = [state_h, state_c]
+encoderInputs = Input(shape=inputCharacters.shape)
+encoderLSTM = LSTM(latent_dim, return_state=True)
+encoderOutputs, hiddenStates, CStates = encoderLSTM(encoderInputs)
+encoderStates = [hiddenStates, CStates]
 
-decoder_inputs = Input(shape=(None, num_decoder_tokens))
-decoder_lstm = LSTM(latent_dim, return_sequences=True, return_state=True)
-decoder_outputs, _, _ = decoder_lstm(decoder_inputs,
-                                     initial_state=encoder_states)
-decoder_dense = Dense(num_decoder_tokens, activation='softmax')
-decoder_outputs = decoder_dense(decoder_outputs)
+decoderInputs = Input(shape=targetCharacters.shape)
+decoderLSTM = LSTM(latent_dim, return_state=True, return_sequences=True)
+decoderOutputs, _, _ = decoderLSTM(decoderInputs, initial_state=encoderStates)
 
-model = Model(inputs=[encoder_inputs, decoder_inputs],
-              outputs=decoder_outputs)
-
-model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+model = Model(inputs=[encoderInputs, decoderInputs], outputs=decoderOutputs)
+model.compile(optimizer='Adam', kernel_regularizer=regularizers.l2(0.01))
 model.summary()
 
-model.fit([encoder_input_data, decoder_input_data], decoder_target_data,
-          batch_size=batch_size,
-          epochs=epochs,
-          validation_split=0.2)
-model.save('/results/seq2seq_eng-ger.h5')
+model.fit([inputCharacters, targetCharacters], targetCharacters, batch_size=batch_size, epochs=epochs, validation_split=0.2)
+model.save('../Models/seq2seq.h5')
+
+# decoderDense = Dense(len(tar))
+        # if innerValue not in targetCharacters:
+        #     targetCharacters.add(innerValue)
+# inputCharacters = np.array(inputCharacters).reshape((len(inputCharacters), length, 1))
+# targetCharacters = np.array(targetCharacters).reshape((len(targetCharacters), length, 1))
+# print(np.array(inputCharacters).shape)
+# print(np.array(targetCharacters).shape)
+# print(len(inputCharacters[0]))
+
+# max_encoder_seq_length = max([len(txt) for txt in input_train_data])
+# max_decoder_seq_length = max([len(txt) for txt in target_train_data])
+# num_encoder_tokens = len(inputCharacters)
+# num_decoder_tokens = len(targetCharacters)
+#
+# print(num_encoder_tokens)
+# print(num_decoder_tokens)
+# input_token_index = dict(
+#   [(char, i) for i, char in enumerate(inputCharacters)])
+# target_token_index = dict(
+#   [(char, i) for i, char in enumerate(targetCharacters)])
+# print(len(input_token_index))
+
+# encoder_input_data = np.zeros(
+#   (len(input_train_data), max_encoder_seq_length, num_encoder_tokens),
+#   dtype='float32')
+# decoder_input_data = np.zeros(
+#   (len(input_train_data), max_decoder_seq_length, num_decoder_tokens),
+#   dtype='float32')
+# decoder_target_data = np.zeros(
+#   (len(input_train_data), max_decoder_seq_length, num_decoder_tokens),
+#   dtype='float32')
+# print(encoder_input_data.shape)
+# print(decoder_input_data.shape)
+# print(decoder_target_data.shape)
+#
+# for i, (input_train_data, target_train_data) in enumerate(zip(input_train_data, target_train_data)):
+#     for t, char in enumerate(input_train_data):
+#         encoder_input_data[i, t, input_token_index[char]] = 1
+#     for t, char in enumerate(target_train_data):
+#     #     decoder_target_data is ahead of decoder_input_data by one timestep
+#         decoder_input_data[i, t, target_token_index[char]] = 1
+#         if t > 0:
+#         #decoder_target_data will be ahead by one timestep
+#         # and will not include the start character.
+#             decoder_target_data[i, t - 1, target_token_index[char]] = 1
+#
+# print(encoder_input_data[2].shape)
+# print(decoder_target_data[2].shape)
+
+# batch_size = 64  # batch size for training
+# epochs = 100  # number of epochs to train for
+# latent_dim = 256  # latent dimensionality of the encoding space
+
+# encoder_inputs = Input(shape=(None, num_encoder_tokens))
+# encoder = LSTM(latent_dim, return_state=True)
+# encoder_outputs, state_h, state_c = encoder(encoder_inputs)
+# encoder_states = [state_h, state_c]
+#
+# decoder_inputs = Input(shape=(None, num_decoder_tokens))
+# decoder_lstm = LSTM(latent_dim, return_sequences=True, return_state=True)
+# decoder_outputs, _, _ = decoder_lstm(decoder_inputs,
+#                                      initial_state=encoder_states)
+# decoder_dense = Dense(num_decoder_tokens, activation='softmax')
+# decoder_outputs = decoder_dense(decoder_outputs)
+#
+# model = Model(inputs=[encoder_inputs, decoder_inputs],
+#               outputs=decoder_outputs)
+#
+# model.compile(optimizer='Adam', kernel_regularizer=regularizers.l2(0.01))
+# model.summary()
+#
+# model.fit([encoder_input_data, decoder_input_data], decoder_target_data,
+#           batch_size=batch_size,
+#           epochs=epochs,
+#           validation_split=0.2)
+# model.save('../Models/seq2seq.h5')
